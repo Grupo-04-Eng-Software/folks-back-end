77cdf1460b6c8d4d15b36a5be48320bf5701ce7e
Lucas Leite Lessa <54894793+LucasLeiteLessa@users.noreply.github.com>
2026-04-09 03:51:03 -0300

    Initial commit

---
+++ src/main/java/faculdade/donaduzzi/folksflowbackend/FolksFlowBackEndApplication.java
@@ -0,0 +1,13 @@
+package faculdade.donaduzzi.folksflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolksFlowBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolksFlowBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/faculdade/donaduzzi/folksflowbackend/FolksFlowBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package faculdade.donaduzzi.folksflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolksFlowBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

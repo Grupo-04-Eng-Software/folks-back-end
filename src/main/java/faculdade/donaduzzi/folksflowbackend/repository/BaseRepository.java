658a94e5959e91a860daf4890aa1681811b474ba
Lucas Leite Lessa <54894793+LucasLeiteLessa@users.noreply.github.com>
2026-04-12 00:40:44 -0300

    feat(repository): add @Modifying and @Transactional annotations to BaseRepository UPDATE methods

--- src/main/java/faculdade/donaduzzi/folksflowbackend/repository/BaseRepository.java
+++ src/main/java/faculdade/donaduzzi/folksflowbackend/repository/BaseRepository.java
@@ -1,9 +1,11 @@
 package faculdade.donaduzzi.folksflowbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,15 +13,4 @@ import java.util.Optional;
 @NoRepositoryBean
 public interface BaseRepository<E, ID> extends JpaRepository<E, ID> {
 
-    @Query ("SELECT e FROM #{entityName} e Where e.isActive = true")
-    List<E> findAllActive();
-
-    @Query("SELECT e FROM #{#entityName} e WHERE e.id = :id AND e.isActive = true")
-    Optional<E> findByIdActive(@Param("id") ID id);
-
-    @Query("UPDATE #{#entityName} e SET e.isActive = false WHERE e.id = :id")
-    void softDelete(@Param("id") ID id);
-
-    @Query("UPDATE #{#entityName} e SET e.isActive = true WHERE e.id = :id")
-    void reactivate(@Param("id") ID id);
 }

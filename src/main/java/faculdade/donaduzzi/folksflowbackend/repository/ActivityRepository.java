0698439985f9c66ce4681975b915a41555454b9c
Lucas Leite Lessa <54894793+LucasLeiteLessa@users.noreply.github.com>
2026-05-13 19:52:12 -0300

    fix: ActivityRepository findByTaskId

--- src/main/java/faculdade/donaduzzi/folksflowbackend/repository/ActivityRepository.java
+++ src/main/java/faculdade/donaduzzi/folksflowbackend/repository/ActivityRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface ActivityRepository extends BaseRepository<Activity, Integer> {
 
 
-    List<Activity> findActivitiesByTaskId(Integer taskTaskId);
+    List<Activity> findActivitiesByTasktask_id(Integer taskTaskId);
 
 
 }
